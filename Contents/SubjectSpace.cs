6ddbd8641fff3415c69057200339f8f3abfd0885
1506022022 <88864717+1506022022@users.noreply.github.com>
2024-02-27 13:56:11 +0900

    Update SubjectSpace.cs

--- Contents/SubjectSpace.cs
+++ Contents/SubjectSpace.cs
@@ -6,8 +6,9 @@ using Valve.VR.InteractionSystem;
 
 public class SubjectSpace : MonoBehaviour
 {
-    const string SubjectTargetTag = "Glass";
-    public UnityEvent subjectEvent;
+    
+    const string SubjectTargetTag = "Glass";    // 이벤트를 처리할 대상
+    public UnityEvent subjectEvent;             // 발동시킬 이벤트
 
     void OnTriggerEnter(Collider other)
     {
@@ -18,14 +19,17 @@ public class SubjectSpace : MonoBehaviour
     {
         if (other.tag != SubjectTargetTag) return;
 
+        // 대상으로 부터 정보를 받아온다.
         var target = other.gameObject;
         var targetRigid = target.GetComponent<Rigidbody>();
         var targetInteractable = target.GetComponent<Interactable>();
         var attachedToHand = targetInteractable?.attachedToHand;
 
+        // 대상을 자유로운 상태로 만든다.
         if (Checker.Exist(targetInteractable, attachedToHand)) DetacchToHand();
         if (targetRigid) NonKinematicTarget();
 
+        // 제출 이벤트 발동
         if(subjectEvent.GetPersistentEventCount() > 0) subjectEvent.Invoke();
 
         #region localFunc
